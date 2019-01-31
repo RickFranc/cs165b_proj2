0661e3b87303553724c1e47c23033828ee6b8361
Rick Franc <nfranc@umail.ucsb.edu>
2019-01-31 09:53:48 -0800

    Tests pass

--- Logistic-SGD/sgd.py
+++ Logistic-SGD/sgd.py
@@ -1,34 +1,42 @@
 from math import exp
 import random
 
-# TODO: Calculate logistic
 def logistic(x):
-    return 1.
+    return 1./(1.+exp(-x))
 
-# TODO: Calculate dot product of two lists
 def dot(x, y):
     s = 0
+    for i in range(len(x)):
+        s += x[i]*y[i]
     return s
 
-# TODO: Calculate prediction based on model
 def predict(model, point):
-    return 0
+    s = dot(model, point['features'])
+    return logistic(s)
+    
 
-# TODO: Calculate accuracy of predictions on data
 def accuracy(data, predictions):
     correct = 0
+    for i in range(len(data)):
+        correct += (data[i]['label'] == (predictions[i]>=0.5))
     return float(correct)/len(data)
 
 # TODO: Update model using learning rate and L2 regularization
 def update(model, point, rate, lam):
-    pass
-
+    prob = predict(model,point)
+    for i in range(len(model)):
+        model[i] += rate*(point['features'][i]*(point['label']-prob) - lam*model[i])
+        
 def initialize_model(k):
     return [random.gauss(0, 1) for x in range(k)]
 
 # TODO: Train model using training data
 def train(data, epochs, rate, lam):
     model = initialize_model(len(data[0]['features']))
+    for i in range(epochs):
+        for j in range(len(data)):
+            update(model, data[random.randint(0,len(data)-1)], rate, lam)
+        
     return model
         
 def extract_features(raw):
